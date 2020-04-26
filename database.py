8e2850e5b7bcb51628b98c18e07aa6d36066255a
Kyle-PC <kylebrain@gmail.com>
2020-04-25 21:27:41 -0700

    Modify tables to reflect new design requirements

--- database.py
+++ database.py
@@ -5,14 +5,10 @@ DB_FILE_NAME = "database.db"
 DEFAULT_FILE_NAME = "defaults.json"
 
 SEASONS = "seasons"
-SEASONAL_FILTERS = "seasonal_filters"
 SUMMER = "summer"
 WINTER = "winter"
-HIGH = "high"
-LOW = "low"
 START = "start"
 PEAK = "peak"
-LOW = "low"
 
 
 def get_defaults():
@@ -26,34 +22,26 @@ def get_month_day(date_string):
 def initialize_sql():
     conn = sqlite3.connect(DB_FILE_NAME)
 
-    conn.execute('''CREATE TABLE IF NOT EXISTS Seasons (
-                        Season text PRIMARY KEY,
-                        StartMonth int NOT NULL,
-                        StartDay int NOT NULL,
-                        PeakMonth int NOT NULL,
-                        PeakDay int NOT NULL
+    conn.execute('''CREATE TABLE IF NOT EXISTS seasons (
+                        season text PRIMARY KEY,
+                        start_month int NOT NULL,
+                        start_day int NOT NULL,
+                        peak_month int NOT NULL,
+                        peak_day int NOT NULL
                     )''')
 
-    conn.execute('''CREATE TABLE IF NOT EXISTS Seasonal (
-                        Season text PRIMARY KEY,
-                        Level text NOT NULL,
-                        Speed int NOT NULL,
-                        StartTime time NOT NULL,
-                        EndTime time NOT NULL,
-                        FOREIGN KEY (Season) REFERENCES Seasons (Season)
+    conn.execute('''CREATE TABLE IF NOT EXISTS programs (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        speed int NOT NULL,
+                        start_time time NOT NULL,
+                        summer_duration time NOT NULL,
+                        winter_duration time NOT NULL
                     )''')
 
     defaults = get_defaults()
 
     conn.execute('''INSERT OR IGNORE INTO Seasons VALUES (?, ?, ?, ?, ?)''', (SUMMER, *get_month_day(defaults[SEASONS][SUMMER][START]), *get_month_day(defaults[SEASONS][SUMMER][PEAK])))
     conn.execute('''INSERT OR IGNORE INTO Seasons VALUES (?, ?, ?, ?, ?)''', (WINTER, *get_month_day(defaults[SEASONS][WINTER][START]), *get_month_day(defaults[SEASONS][WINTER][PEAK])))
-    # conn.execute('''INSERT INTO Seasonal VALUES ('summer', 'high', 8, '08:00', '11:00')''')
-
-    conn.execute('''CREATE TABLE IF NOT EXISTS Override (
-                        Speed int NOT NULL,
-                        StartTime time NOT NULL,
-                        EndTime time NOT NULL
-                    )''')
 
     conn.commit()
 
